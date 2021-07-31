7edc4f68ea2f577eab9dd2d0a77c3fbdbef94065
Paola62010 <paola62010@gmail.com>
2021-07-31 12:10:22 +0000

    Add function to get a random word based on difficulty

--- run.py
+++ run.py
@@ -2,8 +2,9 @@ import random
 
 easy_words = ["able", "acid", "aged", "also", "area", "army"]
 medium_words = ["breath", "bridge", "bright", "broken", "budget", "burden"]
-hard_words = ["enormous", "entirely", "entrance", "envelope", "equality", 
+hard_words = ["enormous", "entirely", "entrance", "envelope", "equality",
               "equation"]
+word = ""
 
 images = ['''
   +---+
@@ -71,5 +72,27 @@ hangman_title = """
 """
 
 print(hangman_title)
-print(images[6])
-hangman_title.rstrip()
+print("Welcome to Hangman! Guess all the letters and reveal the word!\n")
+
+
+def get_word():
+    global word
+    difficulty = input("Select game difficulty: easy, medium, hard\n")
+    if difficulty == "easy":
+        word = random.choice(easy_words)
+    elif difficulty == "medium":
+        word = random.choice(medium_words)
+    elif difficulty == "hard":
+        word = random.choice(hard_words)
+    else:
+        print("Input not valid, please select a valid option")
+        get_word()
+    return word
+        
+
+def main():
+    get_word()
+    print(word)
+
+
+main()
