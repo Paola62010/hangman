9187686d80ba0b49768a3242d16987c10b15b8c8
Paola62010 <paola62010@gmail.com>
2021-08-01 10:30:12 +0000

    Correct play function to return proper message for incorrect and correct guess

--- run.py
+++ run.py
@@ -11,7 +11,7 @@ lives = 7
 guessed = []
 missed = []
 game_complete = False
-word = ""
+word = "able"
 
 
 def get_word():
@@ -36,22 +36,48 @@ def show_status(word):
 
 
 def play():
+    
+    global guessed
+    global missed
+    global lives
     global game_complete
-    while game_complete is False:
-        guess = input("Guess a letter or the entire word: \n").upper()
-        if guess == word:
-            game_complete = True
-            print("Congratulations")
-        elif guess in word:
-            print(f"Well done, {guess} is part of the word")
+    while game_complete is False and lives > 0:
+        guess = input("Guess a letter or the entire word: \n").lower()
+        if len(guess) == 1 and guess.isalpha():
+            if guess in word and len(guessed) != len(word):
+                print(f"Well done, {guess} is part of the word.")
+                guessed.append(guess)
+                print(f"Missed: {missed}")
+                print(f"Lives remaining: {lives}")
+            elif guess in word and len(guessed) == len(word):
+                print("Congratularions, you guessed the word")
+                game_complete = True
+            elif guess not in word and lives != 1:
+                print(f"Sorry, {guess} is not part of the word. Try again.")
+                missed.append(guess)
+                lives -= 1
+                print(f"Missed: {missed}")
+                print(f"Lives remaining: {lives}")
+            else:
+                print(f"Sorry, the correct word was {word}")
+                game_complete = True
+        elif guess == word and guess.isalpha():
+            if guess == word:
+                print("Congratulations! You guessed the word!")
+                game_complete = True
+        elif guess != word:
+            print(f"Sorry, {guess} is not the correct word")
+            lives -= 1
+            print(f"Lives remaining: {lives}")
+
         else:
-            print("You lost")
+            print("Invalid input, only letters are accepted")
 
 
 def main():
     print(hangman_title)
     print("Welcome to Hangman! Guess all the letters and reveal the word!\n")
-    word = get_word()
+    #word = get_word()
     show_status(word)
     play()
 
